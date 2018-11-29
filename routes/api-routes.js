0c3ff3b9f1b355fb9d94a92f16edcf936b997f23
raywon123 <raymond.w.won@gmail.com>
2018-11-29 12:21:57 -0600

    add routes to login, home, manager and profile pages

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -37,58 +37,24 @@ db.quiz.hasMany(db.result, {
 // =============================================================
 module.exports = function (app) {
 
-    // GET - select all (home)
-    app.get("/", function (req, res) {
-        // retrieve all the data from the database and res.json them
-        // back to the user
-        db.burgerseq
-            .findAll().then(data => {
-
-                // then name "burgers" is the same in the file index.handlebars
-                var obj = {
-                    burgers: data
-                };
-
-                res.render("index", obj);
-
-            });
+    app.post("/api/testuser", function (req, res) {
+        console.log(req.body);
+    
+        if (req.body.email === "test1@test.com") {
+            isManager = false;
+        }
+    
+        if (req.body.email === "test2@test.com") {
+            isManager = true;
+        }
+    
+        var obj = {
+            email: req.body.email,
+            isManager: isManager
+        }
+        res.json(obj);
     });
-
-
-    // GET route for select all 
-    app.get("/api/burgers", function (req, res) {
-
-        db.burgerseq
-            .findAll().then(result => res.json(result));
-    });
-
-    // GET - select one
-    app.get("/api/burgers/:id", function (req, res) {
-
-        db.burgerseq
-            .findOne({ where: { id: req.params.id } }).then(result => res.json(result));
-    });
-
-
-    // POST - insert
-    app.post("/api/burgers", function (req, res) {
-
-        db.burgerseq
-            .create(req.body).then(results => res.json(results));
-
-    });
-
-    // PUT - update
-    app.put("/api/burgers/:id", function (req, res) {
-        db.burgerseq
-            .update({ devoured: req.body.devoured }, { where: { id: req.params.id } }).then(results => res.json(results));
-    });
-
-    // DELETE delete 
-    app.delete("/api/burgers/:id", function (req, res) {
-        db.burgerseq
-            .destroy({ where: { id: req.params.id } }).then(results => res.json(results));
-    });
+    
 
     // ---- automentor api routes: user
 
