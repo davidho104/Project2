9b4948e547c72cb736359dbbe44ea1c868e04f06
William B. Swift <willbswift@hotmail.com>
2018-11-29 21:06:16 -0600

    now with graphs!

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,6 +9,10 @@ var db = require("../models");
 var Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 
+var sequelize = new Sequelize('automentor_db', 'root', 'righter', {
+    dialect: "mysql"
+});
+
 //Relations (foreign key constraint)
 // -- need to have both hasOne and BelongsTo to avoid reference error
 //    "ReferenceError: [model] is not defined"
@@ -39,24 +43,24 @@ module.exports = function (app) {
 
     app.post("/api/testuser", function (req, res) {
         console.log(req.body);
-    
+
         var isManager;
-        
+
         if (req.body.email === "test1@test.com") {
             isManager = false;
         }
-    
+
         if (req.body.email === "test2@test.com") {
             isManager = true;
         }
-    
+
         var obj = {
             email: req.body.email,
             isManager: isManager
         }
         res.json(obj);
     });
-    
+
 
     // ---- automentor api routes: user
 
@@ -218,4 +222,50 @@ module.exports = function (app) {
         db.result
             .destroy({ where: { id: req.params.id } }).then(results => res.json(results));
     });
+
+    // ---- automentor api routes: Charts
+
+    // are my get(names) causing a problem?
+
+    // Function to collect data for FIRST Graph
+    function getData(callback) {
+
+        sequelize.query("SELECT users.first_name, SUM(results.score) FROM results JOIN users ON results.user_id = users.id GROUP BY users.first_name").spread((results1, metadata) => {
+            // Results will be resulting array and metadata will contain the number of affected rows.
+            console.log("data1 from database");
+            console.log(results1);
+            return callback(results1);
+        })
+    }
+
+    // Sends Individual Scores to html file
+    app.get("/api/userscore", function (req, res) {
+        getData(function (rows1) {
+            console.log("data1 to be sent");
+            console.log(rows1);
+            res.json(rows1);
+        });
+    });
+
+    // Function to collect data for SECOND Graph
+    function getData2(callback) {
+
+        sequelize.query("SELECT quizzes.id, SUM(results.score) FROM results JOIN quizzes ON results.quiz_id = quizzes.id GROUP BY quizzes.id").spread((results2, metadata) => {
+            // Results will be resulting array and metadata will contain the number of affected rows.
+            console.log("data1 from database");
+            console.log(results2);
+            return callback(results2);
+        })
+    }
+
+    // Sends Test Values to html file
+    app.get("/api/quizscore", function (req, res) {
+        getData2(function (rows2) {
+            console.log("data1 to be sent");
+            console.log(rows2);
+            res.json(rows2);
+        });
+    });
+
+
 };
